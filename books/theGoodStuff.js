05301d38efe7501aeff25fd339b1317d6131504f
ttn_js <ttn.js001@gmail.com>
2017-08-13 16:14:55 -0500

    week 2 will be over chapters 4 and 5

--- books/theGoodStuff.js
+++ books/theGoodStuff.js
@@ -79,7 +79,7 @@ console.log("Hello, World!");
 	*/
 	var notANumber = NaN;
 
-	console.log(notANumber === NaN);
+	console.log(notANumber == NaN);
 	/*
 
 	Question: What does Math.Floor(number); do? 
@@ -196,6 +196,124 @@ r
 	OOP
 	https://softwareengineering.stackexchange.com/questions/34584/how-to-explain-oop-concepts-to-a-non-technical-person
 
+	Question: Tell me about .apply?
+
+	RECURSION
+
+	/*
+	   What is Recursion? 
+	   To put it simple, recursion is when a function calls itself. 
+
+	   Three key features of recursion:
+	   1. A termination condition, where the function terminates if there is an error
+	   2. A base case, another termination condition once we reached our goal
+	   3. Recursion, when our function calls itself
+
+	   returns the factorial of a supplied integer:
+	   Whats a factorial? 
+	   To get the factorial of a number you multiply that number by itself minus one until you reach the number one.
+	   ex. factorial of 4 is 4*3*2*1 = 24
+	*/
+
+	function factorial(x) {
+		// TERMINATION
+		if (x < 0) 
+	  		return;
+	  	// BASE CASE
+	  	if (x === 0) 
+	  		return 1;
+	  	// RECURSION
+	  	return x * factorial(x - 1);
+	}
+
+	console.log(factorial(3));
+
+	/*
+		this is how the above function works:
+
+		this function runs as many times it needs to until it reaches 0
+		so if the input is 3, 3*2*1*1 = 6, once x becomes 0 then the functions stops via return.
+		
+		factorial(0) returns 1                 => 1
+		factorial(1) returns 1 * factorial(0)  => 1 * 1
+		factorial(2) returns 2 * factorial(1)  => 2 * 1 * 1
+		factorial(3) returns 3 * factorial(2)  => 3 * 2 * 1 * 1
+
+		3 * 2 * 1 * 1 = 6
+
+	*/
+
+	/*
+		inefficient recursive reversing string
+
+
+	*/
+
+	function revStr(str){
+		// BASE CASE & TERMINATION
+	  	if (str === '') 
+	  		return '';
+	  	// RECURSION
+	  	return revStr(str.substr(1)) + str[0];
+	}
+
+	/*
+		revStr('')     returns                ''  => ''
+
+		revStr('t')    returns revStr('') +   't' => '' + 't'
+
+		revStr('at')   returns revStr('t') +  'a' => '' + 't' + 'a'
+
+		revStr('cat')  returns revStr('at') + 'c' => '' + 't' + 'a' + 'c'
+
+		tac
+	*/
+
+	console.log(revStr('cat'));
+
+	/*
+
+	SCOPE
+	Question: What is Scope?
+
+
+	CLOSURE
+	Question: What is Closure?
+
+
+	CALLBACKS
+	Question: What is a Callback and why should we use them? asynchronous and speed
+
+
+	MODULE
+	Question: What is a module? 
+
+	CASCADE
+	Question: What is cascade? 
+
+	CURRY 
+	Question: Does JS have a curry method? No What is Curry? 
+
+	MEMOIZATION
+	Question: What is Memoization?
+
+	*** Chapter 5 : Inheritance
+
+	Question: Can someone talk to me about the diff between classical languages and JS in terms of inheritance? 
+	In classical languages, objects are instances of classes where a class can inherit fromanother class. 
+	Javascript is a prototypal language where object inheret directly from other objects. 
+
 */ 
+
+/*
+
+Week 2 Chapter 4 & 5 
+
+Big Topics : OOP, Prototypes
+
+
+
+
+*/
 
 
