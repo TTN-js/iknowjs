e4e1f77c58868b5aa7e90b389cef668efaebf1dd
ttn_js <ttn.js001@gmail.com>
2017-08-23 16:33:44 -0500

    adding gameplan, and learning about FTP

---
+++ web_dev_gameplan.js
@@ -0,0 +1,141 @@
+/*
+
+KEYs
+* must learn topics
++ leveling up topics
+- must learn sub-topics
+@ optional but recommended topics
+() selected topics to learn 
+
+QUARTERs - defined learning timelines; broken into 3 month periods reflective to the US Market; goals are to profit in knowledge.  
+Q1 - January, February and March
+Q2 - April, May and June
+Q3 - July, August and September 
+Q4 - October, November and December
+
+
+Required To Know Technologies - Developer List 
+
+* Learn How Client & Server Talk to Each Other
+	
+	* FTP (Protocols) & Web Host Setup
+		FTP - stands for File Transfer Protocal
+			Used to transfer files between computers and networks/account via online. FTP is a set of conventions that can be used by an "electronic communications system" to transfer files.
+
+			There are many types of FTP protocols. 
+			Browsers only use one type of FTP: 
+				
+			FTPS - works the same as FTP but encrypts everything, most servers use this now. 
+				
+			There are other protocols that collectively make up the internet. ex. IMAP, POP - send and recieve messages, XMPP - used for instant messages, DNS - used to translate domain names into IP addresses.
+
+			How FTP Works
+			FTP servers offer access to a firectory, with sub-directories. Users connect to these servers with an FTP client. 
+
+			FTP clients - software that lets you download and upload files from and to a server. 
+
+			FTP is a way of moving information from the computer you're working on to the server where a website is hosted. It is also used as a way to share files. Upload a file to an ftp server, then share a link with another person. 
+
+			What does FTP look like? 
+			It looks like the other files on your computer. There is a hierarchial folder structure that you can explore. 
+
+			Is FTP secure? 
+			Not by design, meaning FTP transfers aren't encrypted so thing can be intercepted via packet sniffing. 
+
+			HTTP/HTTPS
+		
+
+		Web Hosting - activity or business of providing space and access for websites. 
+	
+
+
+* Basic Terminal Usage
+
+* Basic SSH
+
+* Git / GitHub
+	- 
+
+
+* RESTful Web Services / GET POST PUT DELETE request
+	- APIs
+
+
+@ Learn to Research
+@ Data Structures & Algorithms 
+@ Character Encodings
+
+*/
+
+
+/*
+Road Map Break Down
+
+1) Lets start with Front End Engineering and UX/UI Design
+
+* Front-End Development 
+	Q3 2017 
+
+	- (HTML)
+	- (CSS)
+		+ Precompilers: (SASS) / LESS / Stylus
+		+ Frameworks: (Bootstrap) / (Foundation) / (Materialize) 
+		+ Responsive Design 
+
+	- JavaScript - use this to change HTML and CSS
+		+ ES6/ES2015
+		+ Babel
+		@ Task Runners : Gulp / Grunt
+		+ Dependency Management : Browserify / (Webpack)
+		@ Front-End Build Tools : Bower / package managment / Yeoman.io
+		+ JS Frameworks : 
+
+			- (React) :
+				- Mobx :
+					small applications
+				- (Flux)
+				- Redux
+				- Relay :
+					big applications
+				- create-react-app : 
+					help spin up a quick react app
+
+			- (Angular)
+			- Ember
+			- Vue
+
+			Challenge Yourself 
+			- Clojurescript
+			- Elm
+
+			Unit Testing
+			- Mocha
+			- Jasmine
+			- Karma
+			- enzyme:
+				test react 
+			
+		- jQuery
+
+* UX/UI Deign
+	Q4 2017
+
+
+
+2) Back End Development 
+
+
+
+
+
+
+
+*/
+
+
+/*
+My Learning Style
+
+As a visual learner and to better my learning capabilites I must Read, Take Notes, Reread Notes & Research, Teach. 
+
+*/
